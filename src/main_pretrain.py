584c0120e7e137161081c947de242800603fc056
Akihiro Nakano <apollonia.an@gmail.com>
2024-02-17 17:32:04 +0900

    update config

--- src/main_pretrain.py
+++ src/main_pretrain.py
@@ -3,7 +3,7 @@ from omegaconf import DictConfig
 
 from trainer_pretrain import Trainer
 
-@hydra.main(config_path="../config", config_name="trainer")
+@hydra.main(config_path="../config/ocrl", config_name="trainer_oc_pretrain")
 def main(cfg: DictConfig):
     trainer = Trainer(cfg)
     trainer.run()
--- src/main_slate.py
+++ src/main_slate.py
@@ -3,7 +3,7 @@ from omegaconf import DictConfig
 
 from trainer_slate import Trainer
 
-@hydra.main(config_path="../config", config_name="trainer")
+@hydra.main(config_path="../config/ocrl", config_name="trainer_vanilla_slate")
 def main(cfg: DictConfig):
     trainer = Trainer(cfg)
     trainer.run()
