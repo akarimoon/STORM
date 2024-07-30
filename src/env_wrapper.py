7683bfbee6eafeeb3203dbca0fd92551119a1fe8
Akihiro Nakano <apollonia.an@gmail.com>
2024-07-30 17:23:17 +0900

    feat: debug env

--- src/env_wrapper.py
+++ src/env_wrapper.py
@@ -1,10 +1,14 @@
-import gymnasium
-import numpy as np
+import argparse
 from collections import deque
+
+import numpy as np
+import torch
+from torchvision.utils import save_image
 import cv2
 from einops import rearrange
-import copy
+import gymnasium
 
+from envs import TargetEnv
 
 class LifeLossInfo(gymnasium.Wrapper):
     def __init__(self, env):
@@ -95,6 +99,10 @@ def build_single_env(env_name, image_size):
     env = AtariPreprocessing(env, screen_size=image_size, grayscale_obs=False)
     return env
 
+def build_single_ocrl_env(env_config, image_size, seed, max_step=1000):
+    env = TargetEnv(env_config, seed)
+    env = OCRLMaxStepWrapper(env, max_step=max_step)
+    return env
 
 def build_vec_env(env_list, image_size, num_envs):
     # lambda pitfall refs to: https://python.plainenglish.io/python-pitfalls-with-variable-capture-dcfc113f39b7
@@ -111,19 +119,58 @@ def build_vec_env(env_list, image_size, num_envs):
 
 
 if __name__ == "__main__":
-    vec_env, vec_env_names = build_vec_env(['ALE/Pong-v5', 'ALE/IceHockey-v5', 'ALE/Breakout-v5', 'ALE/Tennis-v5'], 64, num_envs=8)
-    current_obs, _ = vec_env.reset()
-    while True:
-        action = vec_env.action_space.sample()
-        obs, reward, done, truncated, info = vec_env.step(action)
-        # done = done or truncated
-        if done.any():
-            print("---------")
-            print(reward)
-            print(info["episode_frame_number"])
-        cv2.imshow("Pong", current_obs[0])
-        cv2.imshow("IceHockey", current_obs[2])
-        cv2.imshow("Breakout", current_obs[4])
-        cv2.imshow("Tennis", current_obs[6])
-        cv2.waitKey(40)
-        current_obs = obs
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env_type", type=str, default="atari", choices=["atari", "block"])
+    args = parser.parse_args()
+
+    if args.env_type == "atari":
+        vec_env, vec_env_names = build_vec_env(['ALE/Pong-v5', 'ALE/IceHockey-v5', 'ALE/Breakout-v5', 'ALE/Tennis-v5'], 64, num_envs=8)
+        current_obs, _ = vec_env.reset()
+        while True:
+            action = vec_env.action_space.sample()
+            obs, reward, done, truncated, info = vec_env.step(action)
+            # done = done or truncated
+            if done.any():
+                print("---------")
+                print(reward)
+                print(info["episode_frame_number"])
+            cv2.imshow("Pong", current_obs[0])
+            cv2.imshow("IceHockey", current_obs[2])
+            cv2.imshow("Breakout", current_obs[4])
+            cv2.imshow("Tennis", current_obs[6])
+            cv2.waitKey(40)
+            current_obs = obs
+    elif args.env_type == "block":
+        import matplotlib.pyplot as plt
+        from omegaconf import OmegaConf
+
+        config = OmegaConf.load("config/ocrl/env_config/target-debug.yaml")
+        env = build_single_ocrl_env(config, 64, seed=0)
+
+        def pad(x, padding_length_left, padding_length_right):
+            pad_right = torch.nn.functional.pad(x, [0 for _ in range(2 * x.ndim - 1)] + [padding_length_right]) if padding_length_right > 0 else x
+            return torch.nn.functional.pad(pad_right, [0 for _ in range(2 * x.ndim - 2)] + [padding_length_left, 0]) if padding_length_left > 0 else pad_right
+
+        all_episodes, episode_length = [], []
+        max_episode_length = 50
+        num_save = 10
+        for _ in range(100):
+            episode = []
+
+            current_obs, current_info = env.reset()
+            done = False
+            while not done:
+                action = env.action_space.sample()
+                obs, reward, done, truncated, info = env.step(action)
+                if done:
+                    episode_length.append(info["episode_frame_number"])
+                episode.append(current_obs[0])
+                current_obs = obs
+            all_episodes.append(pad(torch.tensor(episode[:max_episode_length]), 0, max_episode_length - len(episode)))
+
+        all_episodes = torch.stack(all_episodes)[:num_save] / 255.0
+        all_episodes = rearrange(all_episodes, 'b t h w c -> (b t) c h w')
+        save_image(all_episodes, "block_env.png", nrow=max_episode_length, pad_value=1.0)
+
+        plt.hist(episode_length, bins=20)
+        plt.savefig("episode_length.png")
